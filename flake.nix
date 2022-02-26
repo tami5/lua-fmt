32c66f45fecbff5c17cc425d604a96a5d89ed236
tami5 <kkharji@protonmail.com>
2022-02-26 11:36:08 +0300

    fix(nix): use $CC

--- flake.nix
+++ flake.nix
@@ -1,30 +1,31 @@
 {
-  inputs.nixlua = "path://Users/tami5/repos/nix/nixlua";
-  outputs = { nixlua }:
+  inputs.nixlua.url = "path:///Users/tami5/repos/nix/nixlua";
+  outputs = { self, nixlua }:
     nixlua.mkOutputs (pkgs:
       with pkgs; rec {
         pname = "fmt";
         defaultVersion = "lua5_4";
         version = "master";
         build = lua: prev: {
+          src = ./.;
           buildInputs = [ ];
           nativeBuildInputs = [ ];
           propagatedBuildInputs = [ ];
           buildPhase = with stdenv;
             if isDarwin then
-              "gcc -o fmt.so -O3 -fPIC -shared -undefined dynamic_lookup lfmt.c"
+              "$CC -o fmt.so -O3 -fPIC -shared -undefined dynamic_lookup lfmt.c"
             else
-              "gcc -o fmt.so -O3 -fPIC -shared lfmt.c" "\n\n";
+              "$CC -o fmt.so -O3 -fPIC -shared lfmt.c" "\n\n";
           installPhase = ''
             TARGET="$out/lib/lua/${lua.lua.luaversion}"
             mkdir -p $TARGET
             cp ${pname}.so $TARGET
           '';
-          checkPhase = ''
-            MSG=${lua} -e 'require"${pname}"("{}", "hello")'
-            [[ $MSG = "hello" ]]
-          '';
-          doCheck = false;
+          # FIXME
+          # checkPhase = ''
+          #   # ${lua.bin} -e 'print(require"${pname}"("{}", "hello"))'
+          # '';
+          # doCheck = true;
         };
         shell = {
           luaEnv = lua:
